d5dbad2e4a155765f6284b954382c51baf2e3395
Martin <martin.smith1994@outlook.com>
2018-11-01 00:32:14 +0000

    Updates

---
+++ precache-manifest.27d26fe37eb3ef7ea28a0a9b5fbdfb8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc1a83417ae1bfba955f",
+    "url": "/contactManager/static/css/main.395b9295.chunk.css"
+  },
+  {
+    "revision": "fc1a83417ae1bfba955f",
+    "url": "/contactManager/static/js/main.fc1a8341.chunk.js"
+  },
+  {
+    "revision": "b958c44c822e01f9f3b9",
+    "url": "/contactManager/static/css/1.a459523b.chunk.css"
+  },
+  {
+    "revision": "b958c44c822e01f9f3b9",
+    "url": "/contactManager/static/js/1.b958c44c.chunk.js"
+  },
+  {
+    "revision": "c9c7142bbfeec04feca1",
+    "url": "/contactManager/static/js/runtime~main.c9c7142b.js"
+  },
+  {
+    "revision": "daa15dfd7859b28038dc89ab744dc272",
+    "url": "/contactManager/index.html"
+  }
+];
